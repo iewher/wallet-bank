dcab57aa5074b3d8069e907a79d4cde49f81201a
iewher <goshagang@mail.ru>
2024-03-08 12:00:41 +0500

    feat: add component primary button

---
+++ src/components/buttons/index.tsx
@@ -0,0 +1,20 @@
+import { FiLoader } from "react-icons/fi";
+import styles from "./index.module.scss";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  loading?: boolean;
+  icon?: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'button'>
+
+export const PrimaryButton = (props: ButtonProps) => {
+  const { children, loading, icon } = props;
+
+  return (
+    <button className={styles.Button}>
+      {loading && <FiLoader />}
+      {!loading && icon}
+      {children}
+    </button>
+  );
+};
