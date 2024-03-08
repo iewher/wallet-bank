3bb922e436e8e877a80bd3c9519b642eb2f6022a
iewher <goshagang@mail.ru>
2024-03-08 11:39:44 +0500

    fix: header move to primary buttons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,42 +1,42 @@
 "use client";
 
-import React, { useState } from "react";
-import logo from "../../../public/logo.svg";
-import { AiOutlineUser } from "react-icons/ai";
-import { Switch } from "antd";
-import { Badge } from "antd";
-import { Modal } from "antd";
+import { useState } from "react";
+import { Badge, Modal, Switch } from "antd";
+import { FiSettings, FiUser } from "react-icons/fi";
+import { PrimaryButton } from "../buttons";
 import styles from "./index.module.scss";
 
 // interface HeaderProps {}
 
 const Header: React.FunctionComponent = () => {
-  const [openModalPush, setOpenModalPush] = useState<boolean>(false);
-  const [openModalSetting, setOpenModalSetting] = useState<boolean>(false);
-  const [openModalAuth, setOpenModalAuth] = useState<boolean>(false);
+  const [openModalPush, setOpenModalPush] = useState(false);
+  const [openModalSetting, setOpenModalSetting] = useState(false);
+  const [openModalAuth, setOpenModalAuth] = useState(false);
 
   return (
     <div className={styles.Header}>
-      <div className={styles.Container}>
-        <div className={styles.Buttons}>
-          <img src={logo} alt="logo" />
-          <button>Overview</button>
-          <button>Transaction</button>
-          <button>Invoice</button>
-          <button>Activity</button>
+      <div className={styles.Logo}>
+        {/* TODO: Вернуть логотип*/}
+        <img alt="logo" />
+      </div>
+      <div className={styles.Navigations}>
+        <PrimaryButton>Overview</PrimaryButton>
+        <PrimaryButton>Transaction</PrimaryButton>
+        <PrimaryButton>Invoice</PrimaryButton>
+        <PrimaryButton>Activity</PrimaryButton>
+      </div>
+      <div className={styles.Actions}>
+        <div className={styles.Switch}>
+          <Switch />
         </div>
-        <div className={styles.Settings}>
-          <div className={styles.Switch}>
-            <Switch />
-          </div>
-          <Badge count={5}>
-            <button onClick={() => setOpenModalPush(true)}></button>
-          </Badge>
-          <button onClick={() => setOpenModalSetting(true)}></button>
-          <button className="user" onClick={() => setOpenModalAuth(true)}>
-            <AiOutlineUser size={"16px"} color="#D6D7DB" />
-          </button>
-        </div>
+        <Badge count={5}>
+          <PrimaryButton onClick={() => setOpenModalPush(true)}>
+            <FiSettings />
+          </PrimaryButton>
+        </Badge>
+        <PrimaryButton onClick={() => setOpenModalAuth(true)}>
+          <FiUser size={"16px"} />
+        </PrimaryButton>
       </div>
       <Modal
         title="Notification"
