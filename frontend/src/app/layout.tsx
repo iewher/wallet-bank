779be4a12f2c10369fadc32b3a9ec66bf2bbdfcd
iewher <goshagang@mail.ru>
2024-03-23 14:17:06 +0500

    feat: add logo for matadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,9 @@ import "./global.scss";
 export const metadata: Metadata = {
   title: "Wallet dashboard",
   description: "Сайт для отслеживания информации по своим крипто-кошелькам",
+  icons: {
+    icon: "/favicon/favicon.png",
+  },
 };
 
 export default function RootLayout({
